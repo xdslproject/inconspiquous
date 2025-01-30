71dd92094937369df511a85f8e11b0448b8edd33
Alex Rice <alexrice999@hotmail.co.uk>
2025-01-30 13:21:56 +0000

    misc: update xdsl

--- inconspiquous/constraints.py
+++ inconspiquous/constraints.py
@@ -1,14 +1,11 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from collections.abc import Set
 from typing import TypeVar
-from xdsl.dialects.builtin import IntAttr
 from xdsl.ir import Attribute, VerifyException
 from xdsl.irdl import (
     ConstraintContext,
     ConstraintVariableType,
     GenericAttrConstraint,
-    InferenceContext,
     IntConstraint,
     VarExtractor,
 )
@@ -52,23 +49,3 @@ class SizedAttributeConstraint(GenericAttrConstraint[SizedAttributeCovT]):
             k: self._Extractor(v)
             for k, v in self.size_constraint.get_length_extractors().items()
         }
-
-
-@dataclass(frozen=True)
-class IntAttrConstraint(GenericAttrConstraint[IntAttr]):
-    """
-    Constrains the value of an IntAttr.
-    """
-
-    int_constraint: IntConstraint
-
-    def verify(self, attr: Attribute, constraint_context: ConstraintContext) -> None:
-        if not isinstance(attr, IntAttr):
-            raise VerifyException(f"attribute {attr} expected to be an IntAttr")
-        self.int_constraint.verify(attr.data, constraint_context)
-
-    def can_infer(self, var_constraint_names: Set[str]) -> bool:
-        return self.int_constraint.can_infer(var_constraint_names)
-
-    def infer(self, context: InferenceContext) -> IntAttr:
-        return IntAttr(self.int_constraint.infer(context))
