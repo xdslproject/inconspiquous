18ca1df5604771b64cfc182d65d0446abadcf9ef
Alex Rice <alexrice999@hotmail.co.uk>
2025-02-05 16:54:19 +0000

    dialects: ConstantMeasurmentOp -> ConstantMeasurementOp (#62)

--- inconspiquous/transforms/canonicalization/qssa.py
+++ inconspiquous/transforms/canonicalization/qssa.py
@@ -5,7 +5,7 @@ from xdsl.pattern_rewriter import (
 )
 
 from inconspiquous.dialects.gate import ComposeGateOp, ConstantGateOp, IdentityGate
-from inconspiquous.dialects.measurement import ConstantMeasurmentOp
+from inconspiquous.dialects.measurement import ConstantMeasurementOp
 from inconspiquous.dialects.qssa import DynMeasureOp, GateOp, MeasureOp, DynGateOp
 
 
@@ -51,7 +51,7 @@ class DynMeasureConst(RewritePattern):
 
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: DynMeasureOp, rewriter: PatternRewriter):
-        if isinstance(owner := op.measurement.owner, ConstantMeasurmentOp):
+        if isinstance(owner := op.measurement.owner, ConstantMeasurementOp):
             rewriter.replace_matched_op(
                 MeasureOp(*op.in_qubits, measurement=owner.measurement)
             )
