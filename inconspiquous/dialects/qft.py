f028974ba5b3f4a96fd9cd0694b4e7bafba89597
Kim Worrall <kimsworrall@gmail.com>
2025-03-04 10:11:15 +0000

    add GateAttrs for the component gates of the QFT

--- inconspiquous/dialects/qft.py
+++ inconspiquous/dialects/qft.py
@@ -1,3 +1,7 @@
+from abc import ABC
+
+
+from inconspiquous.gates.core import SingleQubitGate, TwoQubitGate
 from xdsl.dialects.builtin import IntAttr
 from xdsl.ir import Dialect
 from xdsl.irdl import irdl_attr_definition, ParameterDef
@@ -5,6 +9,66 @@ from xdsl.parser import AttrParser
 from xdsl.printer import Printer
 
 from inconspiquous.gates import GateAttr
+
+
+class Latexable(ABC):
+    def return_latex(
+        self,
+    ) -> list[str]:
+        raise NotImplementedError("Requires latex return")
+
+
+@irdl_attr_definition
+class HadamardGate(SingleQubitGate, Latexable):
+    name = "qft.h"
+
+    def return_latex(
+        self,
+    ):
+        return ["\\gate{H}"]
+
+
+@irdl_attr_definition
+class SwapGate(TwoQubitGate, Latexable):
+    name = "qft.swap"
+
+    def return_latex(self, inq: int, outq: int):
+        if inq < outq:
+            return ["\\swapx", "\\swapx\\qw"]
+        else:
+            return ["\\swapx\\qw", "\\swapx"]
+
+
+@irdl_attr_definition
+class nthrootGate(TwoQubitGate, Latexable):
+    name = "qft.pair"
+
+    nthroot: ParameterDef[IntAttr]
+
+    @property
+    def nthroot(self) -> int:
+        return self.nthroot.data
+
+    def __init__(self, nthroot: int | IntAttr):
+        if not isinstance(nthroot, IntAttr):
+            nthroot = IntAttr(nthroot)
+
+        super().__init__((nthroot,))
+
+    @classmethod
+    def parse_parameters(cls, parser: AttrParser) -> tuple[IntAttr]:
+        with parser.in_angle_brackets():
+            nthroot = parser.parse_integer(allow_negative=False, allow_boolean=False)
+        return (IntAttr.new(nthroot),)
+
+    def print_parameters(self, printer: Printer) -> None:
+        return self.nthroot.print_parameter(printer)
+
+    def return_latex(self, inq: int, outq: int):
+        return [
+            "\\ctrl{" + str(outq - inq) + "}",
+            "\\gate{Z^{1/" + str(self.nthroot) + "}}",
+        ]
 
 
 @irdl_attr_definition
@@ -36,5 +100,5 @@ class QFTAttr(GateAttr):
 QFT = Dialect(
     "qft",
     [],
-    [QFTAttr],
+    [QFTAttr, HadamardGate, nthrootGate, SwapGate],
 )
--- tests/filecheck/dialects/qft/ops.mlir
+++ tests/filecheck/dialects/qft/ops.mlir
@@ -3,3 +3,9 @@
 "test.op"() {gate = #qft.n<5>} : () -> ()
 
 // CHECK: "test.op"() {gate = #qft.n<5>} : () -> ()
+
+// CHECK: %q0 = qubit.alloc
+%q0 = qubit.alloc
+
+// CHECK: qref.gate<#qft.h> %q0
+qref.gate<#qft.h> %q0
