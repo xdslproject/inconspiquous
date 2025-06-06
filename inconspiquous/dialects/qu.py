92e7a44d9078601a3688a1c2c6351443539eeb4c
Tan Jun Liang <43441314+poig@users.noreply.github.com>
2025-06-06 23:17:03 +1000

    Update inconspiquous/dialects/qu.py
    Co-authored-by: Alex Rice <alexrice999@hotmail.co.uk>

--- inconspiquous/dialects/qu.py
+++ inconspiquous/dialects/qu.py
@@ -87,7 +87,7 @@ class ToBitsOp(IRDLOperation):
     assembly_format = "$reg attr-dict `:` type($reg) `->` type($qubits)"
 
     def __init__(self, reg: SSAValue):
-        reg_type = reg.typ
+        reg_type = reg.type
         if not isinstance(reg_type, RegisterType): raise TypeError("Input must be RegisterType")
         super().__init__(operands=[reg], result_types=[[BitType()] * reg_type.size.data])
 
