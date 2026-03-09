aa18871aee375fd90b37221e42643eea92e284f8
Alex Rice <alexrice999@hotmail.co.uk>
2026-03-09 14:02:18 +0000

    testing: (qir) enable qir test (#186)
    whoops

--- tests/test_qir.py
+++ tests/test_qir.py
@@ -4,14 +4,14 @@ from inconspiquous.dialects.qir import QIR, QIROperation, QubitType, ResultType
 
 def test_qir_op_names():
     for op in QIR.operations:
-        assert isinstance(op, QIROperation)
+        assert issubclass(op, QIROperation)
         assert op.__doc__ is not None
         assert op.get_func_name() in op.__doc__
 
 
 def test_qir_op_types():
     for op in QIR.operations:
-        assert isinstance(op, QIROperation)
+        assert issubclass(op, QIROperation)
         op_def = op.get_irdl_definition()
         ty = op.get_func_type()
         assert len(ty.inputs) == len(op_def.operands)
